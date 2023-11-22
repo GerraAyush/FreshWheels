df108e88722e26065e0d1f94ff5f88be9d04fe35
Nilanchal <nilanchalpanda2003@gmail.com>
2023-11-22 15:42:45 +0530

    Added Information Form of every webpage: Driver, Customer, Farmer

--- Driver/views.py
+++ Driver/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def get_driversInfo_page(httpRequest):
+    return render(httpRequest, 'driversInfo.html', {})
--- Farmer/urls.py
+++ Farmer/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = "Farmer"
 urlpatterns = [
-    
+    path('farmersInfo', views.get_farmersInfo_page, name="farmersInfo"),
 ]
